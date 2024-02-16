28a57ce2045c0138d87575913904910b3757dc1d
kevglass <cokeandcode@gmail.com>
2024-02-16 12:58:55 +0000

    abstract the renderer

---
+++ src/canvasRenderer.ts
@@ -0,0 +1,185 @@
+import { Renderer, TileSet, graphics } from "./graphics";
+import { resources } from "./resources";
+
+// This is a very brute force simple renderer. It's just blitting images and text to 
+// a canvas. It's wrapped with a view to replacing it with something decent
+let ctx: CanvasRenderingContext2D;
+const scaledImageCache: Record<string, Record<number, CanvasImageSource>> = {};
+
+export const canvasRenderer: Renderer = {
+    init(canvas: HTMLCanvasElement): Renderer {
+        ctx = canvas.getContext("2d", { alpha: false }) as CanvasRenderingContext2D;
+
+        return canvasRenderer;
+    },
+
+    loadImage(url: string, track = true): HTMLImageElement {
+        if (track) {
+            resources.resourceRequested(url);
+        }
+        const image = new Image();
+        image.src = url;
+        image.onerror = () => {
+            console.log("Failed to load: " + url);
+        }
+        image.onload = () => {
+            image.id = url;
+            scaledImageCache[image.id] = {};
+            scaledImageCache[image.id][image.width + (image.height * 10000)] = image;
+
+            if (track) {
+                resources.resourceLoaded(url);
+            }
+        }
+
+        return image;
+    },
+
+    // load an image and store it with tileset information
+    loadTileSet(url: string, tw: number, th: number): TileSet {
+        resources.resourceRequested(url);
+
+        const image = new Image();
+        image.src = url;
+        image.onerror = () => {
+            console.log("Failed to load: " + url);
+        }
+        image.onload = () => {
+            resources.resourceLoaded(url);
+        }
+
+        return { image, tileWidth: tw, tileHeight: th, tiles: [] };
+    },
+
+    // Draw a single tile from a tile set by default at its natural size
+    drawTile(tiles: TileSet, x: number, y: number, tile: number): void {
+        x = Math.floor(x);
+        y = Math.floor(y);
+    
+        const tw = Math.floor(tiles.image.width / tiles.tileWidth);
+        const tx = (tile % tw) * tiles.tileWidth;
+        const ty = Math.floor(tile / tw) * tiles.tileHeight;
+
+        let tileImage = tiles.tiles[tile];
+        if (!tileImage) {
+            tileImage = tiles.tiles[tile] = document.createElement("canvas");
+            tileImage.width = tiles.tileWidth;
+            tileImage.height = tiles.tileHeight;
+            tileImage.getContext("2d")?.drawImage(tiles.image, tx, ty, tiles.tileWidth, tiles.tileHeight, 0, 0, tiles.tileWidth, tiles.tileHeight);
+        }
+        ctx.drawImage(tileImage, x, y);
+    },
+
+    outlineText(x: number, y: number, str: string, size: number, col: string, outline: string, outlineWidth: number): void {
+        canvasRenderer.drawText(x - outlineWidth, y - outlineWidth, str, size, outline);
+        canvasRenderer.drawText(x + outlineWidth, y - outlineWidth, str, size, outline);
+        canvasRenderer.drawText(x - outlineWidth, y + outlineWidth, str, size, outline);
+        canvasRenderer.drawText(x + outlineWidth, y + outlineWidth, str, size, outline);
+
+        canvasRenderer.drawText(x, y, str, size, col);
+    },
+
+    // draw text at the given location 
+    drawText(x: number, y: number, str: string, size: number, col: string): void {
+        ctx.fillStyle = col;
+        ctx.font = "bold " + size + "px \"Fira Sans\", sans-serif";
+        ctx.fillText(str, x, y);
+    },
+
+    // draw a rectangle outlined to the canvas
+    drawRect(x: number, y: number, width: number, height: number, col: string): void {
+        ctx.fillStyle = col;
+        ctx.fillRect(x, y, width, 1);
+        ctx.fillRect(x, y + height - 1, width, 1);
+        ctx.fillRect(x, y, 1, height);
+        ctx.fillRect(x + width - 1, y, 1, height);
+    },
+
+    // determine the width of a string when rendered at a given size
+    textWidth(text: string, size: number) {
+        ctx.font = "bold " + size + "px \"Fira Sans\", sans-serif";
+        return ctx.measureText(text).width;
+    },
+
+    // draw a string onto the canvas centring it on the screen
+    centerText(text: string, size: number, y: number, col: string): void {
+        const cx = Math.floor(graphics.width() / 2);
+        canvasRenderer.drawText(cx - (canvasRenderer.textWidth(text, size) / 2), y, text, size, col);
+    },
+
+    // fill a rectangle to the canvas
+    fillRect(x: number, y: number, width: number, height: number, col: string) {
+        ctx.fillStyle = col;
+        ctx.fillRect(x, y, width, height);
+    },
+
+    // draw an image to the canvas 
+    drawImage(image: HTMLImageElement, x: number, y: number, width: number, height: number): void {
+        x = Math.floor(x);
+        y = Math.floor(y);
+        width = Math.floor(width);
+        height = Math.floor(height);
+        
+        if (image.id) {
+            if (width === 0) {
+                return;
+            }
+
+            let cachedScaled = scaledImageCache[image.id][width + (height * 10000)];
+            if (!cachedScaled) {
+                cachedScaled = scaledImageCache[image.id][width + (height * 10000)] = document.createElement("canvas");
+                cachedScaled.width = width;
+                cachedScaled.height = height;
+                cachedScaled.getContext("2d")?.drawImage(image, 0, 0, width, height);
+            }
+
+            ctx.drawImage(cachedScaled, x, y);
+        }
+    },
+
+    // store the current 'state' of the canvas. This includes transforms, alphas, clips etc
+    push() {
+        ctx.save();
+    },
+
+    // restore the next 'state' of the canvas on the stack.
+    pop() {
+        ctx.restore();
+    },
+
+    // set the alpha value to use when rendering 
+    alpha(alpha: number): void {
+        ctx.globalAlpha = alpha;
+    },
+
+    // translate the rendering context by a given amount
+    translate(x: number, y: number): void {
+        x = Math.floor(x);
+        y = Math.floor(y);
+
+        ctx.translate(x, y);
+    },
+
+    // scale the rendering context by a given amount
+    scale(x: number, y: number): void {
+        ctx.scale(x, y);
+    },
+
+    rotate(ang: number): void {
+        ctx.rotate(ang);
+    },
+
+    fillCircle(x: number, y: number, radius: number, col: string): void {
+        ctx.fillStyle = col;
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, Math.PI * 2);
+        ctx.fill();
+    },
+
+    halfCircle(x: number, y: number, radius: number, col: string): void {
+        ctx.fillStyle = col;
+        ctx.beginPath();
+        ctx.arc(x, y, radius, Math.PI, 0);
+        ctx.fill();
+    },
+}
--- src/graphics.ts
+++ src/graphics.ts
@@ -1,11 +1,12 @@
+import { canvasRenderer } from "./canvasRenderer";
 import { ResourceListener, resources } from "./resources";
 import { sound } from "./sound";
 
 // This is a very brute force simple renderer. It's just blitting images and text to 
 // a canvas. It's wrapped with a view to replacing it with something decent
+console.log("TOGL 1.01");
 
 const canvas = document.getElementById("gamecanvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d", { alpha: false }) as CanvasRenderingContext2D;
 let eventListener: Game | undefined;
 let mouseDown = false;
 
@@ -110,8 +111,6 @@ canvas.addEventListener("mouseup", (event) => {
     event.stopPropagation();
 });
 
-const scaledImageCache: Record<string, Record<number, CanvasImageSource>> = {};
-
 function loop(game: Game): void {
     // give the utility classes a chance to update based on 
     // screen size etc
@@ -121,6 +120,61 @@ function loop(game: Game): void {
     
     requestAnimationFrame(() => { loop(game) });
 }
+
+export interface Renderer {
+    init(canvas: HTMLCanvasElement): Renderer;
+
+    loadImage(url: string, track: boolean): HTMLImageElement 
+
+    // load an image and store it with tileset information
+    loadTileSet(url: string, tw: number, th: number): TileSet;
+
+    // Draw a single tile from a tile set by default at its natural size
+    drawTile(tiles: TileSet, x: number, y: number, tile: number): void;
+
+    outlineText(x: number, y: number, str: string, size: number, col: string, outline: string, outlineWidth: number): void;
+
+    // draw text at the given location 
+    drawText(x: number, y: number, str: string, size: number, col: string): void;
+
+    // draw a rectangle outlined to the canvas
+    drawRect(x: number, y: number, width: number, height: number, col: string): void;
+
+    // determine the width of a string when rendered at a given size
+    textWidth(text: string, size: number): number;
+
+    // draw a string onto the canvas centring it on the screen
+    centerText(text: string, size: number, y: number, col: string): void;
+
+    // fill a rectangle to the canvas
+    fillRect(x: number, y: number, width: number, height: number, col: string): void;
+
+    // draw an image to the canvas 
+    drawImage(image: HTMLImageElement, x: number, y: number, width: number, height: number): void;
+
+    // store the current 'state' of the canvas. This includes transforms, alphas, clips etc
+    push(): void;
+
+    // restore the next 'state' of the canvas on the stack.
+    pop(): void;
+
+    // set the alpha value to use when rendering 
+    alpha(alpha: number): void;
+
+    // translate the rendering context by a given amount
+    translate(x: number, y: number): void;
+
+    // scale the rendering context by a given amount
+    scale(x: number, y: number): void;
+
+    rotate(ang: number): void;
+
+    fillCircle(x: number, y: number, radius: number, col: string): void;
+
+    halfCircle(x: number, y: number, radius: number, col: string): void;
+}
+
+let currentRenderer: Renderer = canvasRenderer.init(canvas);
 
 export const graphics = {
     // register an event listener for mouse/touch events
@@ -141,97 +195,41 @@ export const graphics = {
     },
 
     loadImage(url: string, track = true): HTMLImageElement {
-        if (track) {
-            resources.resourceRequested(url);
-        }
-        const image = new Image();
-        image.src = url;
-        image.onerror = () => {
-            console.log("Failed to load: " + url);
-        }
-        image.onload = () => {
-            image.id = url;
-            scaledImageCache[image.id] = {};
-            scaledImageCache[image.id][image.width + (image.height * 10000)] = image;
-
-            if (track) {
-                resources.resourceLoaded(url);
-            }
-        }
-
-        return image;
+        return currentRenderer.loadImage(url, track);
     },
 
     // load an image and store it with tileset information
     loadTileSet(url: string, tw: number, th: number): TileSet {
-        resources.resourceRequested(url);
-
-        const image = new Image();
-        image.src = url;
-        image.onerror = () => {
-            console.log("Failed to load: " + url);
-        }
-        image.onload = () => {
-            resources.resourceLoaded(url);
-        }
-
-        return { image, tileWidth: tw, tileHeight: th, tiles: [] };
+        return currentRenderer.loadTileSet(url, tw, th);
     },
 
     // Draw a single tile from a tile set by default at its natural size
     drawTile(tiles: TileSet, x: number, y: number, tile: number): void {
-        x = Math.floor(x);
-        y = Math.floor(y);
-    
-        const tw = Math.floor(tiles.image.width / tiles.tileWidth);
-        const tx = (tile % tw) * tiles.tileWidth;
-        const ty = Math.floor(tile / tw) * tiles.tileHeight;
-
-        let tileImage = tiles.tiles[tile];
-        if (!tileImage) {
-            tileImage = tiles.tiles[tile] = document.createElement("canvas");
-            tileImage.width = tiles.tileWidth;
-            tileImage.height = tiles.tileHeight;
-            tileImage.getContext("2d")?.drawImage(tiles.image, tx, ty, tiles.tileWidth, tiles.tileHeight, 0, 0, tiles.tileWidth, tiles.tileHeight);
-        }
-        ctx.drawImage(tileImage, x, y);
+        currentRenderer.drawTile(tiles, x, y, tile);
     },
 
     outlineText(x: number, y: number, str: string, size: number, col: string, outline: string, outlineWidth: number): void {
-        graphics.drawText(x - outlineWidth, y - outlineWidth, str, size, outline);
-        graphics.drawText(x + outlineWidth, y - outlineWidth, str, size, outline);
-        graphics.drawText(x - outlineWidth, y + outlineWidth, str, size, outline);
-        graphics.drawText(x + outlineWidth, y + outlineWidth, str, size, outline);
-
-        graphics.drawText(x, y, str, size, col);
+        currentRenderer.outlineText(x, y, str, size, col, outline, outlineWidth);
     },
 
     // draw text at the given location 
     drawText(x: number, y: number, str: string, size: number, col: string): void {
-        ctx.fillStyle = col;
-        ctx.font = "bold " + size + "px \"Fira Sans\", sans-serif";
-        ctx.fillText(str, x, y);
+        currentRenderer.drawText(x, y, str, size, col);
     },
 
     // draw a rectangle outlined to the canvas
     drawRect(x: number, y: number, width: number, height: number, col: string): void {
-        ctx.fillStyle = col;
-        ctx.fillRect(x, y, width, 1);
-        ctx.fillRect(x, y + height - 1, width, 1);
-        ctx.fillRect(x, y, 1, height);
-        ctx.fillRect(x + width - 1, y, 1, height);
+        currentRenderer.drawRect(x, y, width, height, col);
     },
 
     // determine the width of a string when rendered at a given size
     textWidth(text: string, size: number) {
-        ctx.font = "bold " + size + "px \"Fira Sans\", sans-serif";
-        return ctx.measureText(text).width;
+        return currentRenderer.textWidth(text, size);
     },
 
     // draw a string onto the canvas centring it on the screen
     centerText(text: string, size: number, y: number, col: string): void {
-        const cx = Math.floor(graphics.width() / 2);
-        graphics.drawText(cx - (graphics.textWidth(text, size) / 2), y, text, size, col);
+        currentRenderer.centerText(text, size, y, col);
     },
 
     // give the graphics to do anything it needs to do per frame
@@ -247,77 +245,48 @@ export const graphics = {
 
     // fill a rectangle to the canvas
     fillRect(x: number, y: number, width: number, height: number, col: string) {
-        ctx.fillStyle = col;
-        ctx.fillRect(x, y, width, height);
+        currentRenderer.fillRect(x, y, width, height, col);
     },
 
     // draw an image to the canvas 
     drawImage(image: HTMLImageElement, x: number, y: number, width: number, height: number): void {
-        x = Math.floor(x);
-        y = Math.floor(y);
-        width = Math.floor(width);
-        height = Math.floor(height);
-        
-        if (image.id) {
-            if (width === 0) {
-                return;
-            }
-
-            let cachedScaled = scaledImageCache[image.id][width + (height * 10000)];
-            if (!cachedScaled) {
-                cachedScaled = scaledImageCache[image.id][width + (height * 10000)] = document.createElement("canvas");
-                cachedScaled.width = width;
-                cachedScaled.height = height;
-                cachedScaled.getContext("2d")?.drawImage(image, 0, 0, width, height);
-            }
-
-            ctx.drawImage(cachedScaled, x, y);
-        }
+        currentRenderer.drawImage(image, x, y, width, height);
     },
 
     // store the current 'state' of the canvas. This includes transforms, alphas, clips etc
     push() {
-        ctx.save();
+        currentRenderer.push();
     },
 
     // restore the next 'state' of the canvas on the stack.
     pop() {
-        ctx.restore();
+        currentRenderer.pop();
     },
 
     // set the alpha value to use when rendering 
     alpha(alpha: number): void {
-        ctx.globalAlpha = alpha;
+        currentRenderer.alpha(alpha);
     },
 
     // translate the rendering context by a given amount
     translate(x: number, y: number): void {
-        x = Math.floor(x);
-        y = Math.floor(y);
-
-        ctx.translate(x, y);
+        currentRenderer.translate(x, y);
     },
 
     // scale the rendering context by a given amount
     scale(x: number, y: number): void {
-        ctx.scale(x, y);
+        currentRenderer.scale(x, y);
     },
 
     rotate(ang: number): void {
-        ctx.rotate(ang);
+        currentRenderer.rotate(ang);
     },
 
     fillCircle(x: number, y: number, radius: number, col: string): void {
-        ctx.fillStyle = col;
-        ctx.beginPath();
-        ctx.arc(x, y, radius, 0, Math.PI * 2);
-        ctx.fill();
+        currentRenderer.fillCircle(x, y, radius, col);
     },
 
     halfCircle(x: number, y: number, radius: number, col: string): void {
-        ctx.fillStyle = col;
-        ctx.beginPath();
-        ctx.arc(x, y, radius, Math.PI, 0);
-        ctx.fill();
+        currentRenderer.halfCircle(x, y, radius, col);
     },
 }
